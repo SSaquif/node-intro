8abb10aee2d54a8677f54dbd6234987e273cfdfb
Sadnan Saquif <ssaquif@gmail.com>
2017-07-07 22:16:32 +0000

    getIssPosition implemented

--- workshop.js
+++ workshop.js
@@ -6,14 +6,43 @@ function getDistance(pos1, pos2) {
 }
 
 function getIssPosition() {
-  return request(/* ... */)
-  .then(
-    function(responce) {
+  //this returns a promise Because --request().then()--
+  return request('http://api.open-notify.org/iss-no.json')
+  .then
+  (
+   //this is the call back function that will return co-ordinates
+   //However since it is inside then, the whole thing will be returned as a promise
+    function(response) //response is the data received from request(url)
+    {
       // Parse as JSON
+      var position = JSON.parse(response);
       // Return object with lat and lng
+      var coordinates = {}; //new Object to store lat and lng
+      coordinates.lat = position.iss_position.latitude;
+      coordinates.lng = position.iss_position.longitude;
+      //console.log(coordinates); // for testing
+      return coordinates;
     }
   )
 }
+//testing
+getIssPosition()
+  .then
+  (
+    function(pos) //position is the return value of getISSPosition()
+    {
+      console.log(pos);
+    }
+  )
+  .catch
+  (
+    function(error) 
+    {
+      console.log("File not found");
+    }
+  );
+
+
 
 function getAddressPosition(address) {
 
